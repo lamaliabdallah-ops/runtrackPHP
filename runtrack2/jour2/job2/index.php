b4c71938e72504f188c20b7ddedfa485b0278796
lamaliabdallah <lamaliabdalah90@gmail.com>
2026-01-06 09:55:38 +0100

    laplateforme

---
+++ runtrack2/jour2/job2/index.php
@@ -0,0 +1,9 @@
+<?php
+// Affichage avec commentaire sur une ligne
+echo "Hello LaPlateforme!"; // Ceci est un commentaire sur une ligne
+
+/* 
+Affichage avec commentaire en bloc
+*/
+echo "Hello LaPlateforme!";
+?>
---
+++ runtrack2/jour2/job3/index.php
@@ -0,0 +1,8 @@
+<?php
+$string =12;
+echo "$string"
+
+
+
+
+?>
