74f70a1c3a400dde101db142555f1a7df77e0d1f
lamaliabdallah <lamaliabdalah90@gmail.com>
2026-01-08 13:32:52 +0100

    jour4

--- jour3/job4/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str = "Dans l'espace, personne ne vous entend crier";
-$compteur = 0;
-
-for ($i = 0; $i < strlen($str); $i++) {
-    $compteur++;
-}
-
-echo $compteur;
--- jour3/job6/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$str = "Les choses que l'on Possède finissent par nous posséder.";
-
-for ($i = strlen($str) - 1; $i >= 0; $i--) {
-    echo $str[$i];
-}
