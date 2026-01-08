d14ccf78c3a78b163ce07b0f4b5f226fbd794451
lamaliabdallah <lamaliabdalah90@gmail.com>
2026-01-08 11:15:54 +0100

    rajouter un dossier

--- runtrack2/jour2/job3/index.php
+++ runtrack2/jour2/job3/index.php
@@ -1,8 +1,49 @@
 <?php
-$string =12;
-echo "$string"
-
-
-
-
+$bool = true;
+$int = 10;
+$string = "Hello";
+$float = 3.14;
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Job 03</title>
+</head>
+<body>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>boolean</td>
+            <td>$bool</td>
+            <td><?php echo $bool; ?></td>
+        </tr>
+        <tr>
+            <td>integer</td>
+            <td>$int</td>
+            <td><?php echo $int; ?></td>
+        </tr>
+        <tr>
+            <td>string</td>
+            <td>$string</td>
+            <td><?php echo $string; ?></td>
+        </tr>
+        <tr>
+            <td>float</td>
+            <td>$float</td>
+            <td><?php echo $float; ?></td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
